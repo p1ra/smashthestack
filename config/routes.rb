0fe07bb9de482cf70ab3bfb294548a790ee221cd
Kelvin White <kelvin@firma8.com>
2012-11-25 20:19:38 -0500

    free web-irc, no registration required

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Smashthestack::Application.routes.draw do
   devise_for :users, path: '', controllers: { sessions: 'sessions', registrations: 'registrations' }, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}, sign_out_via: [:post, :delete, :get]
   root to: "homes#show"
   resource :homes
+  match '/irc', to: 'homes#irc', as: 'irc'
   match '/faq', to: 'homes#faq', as: 'faq'
   match '/contact', to: 'homes#contact', as: 'contact'
   match '/disclaimer', to: 'homes#disclaimer', as: 'disclaimer'
@@ -16,7 +17,6 @@ Smashthestack::Application.routes.draw do
     get 'apfel'
   end
   authenticate do
-    match '/irc', to: 'homes#irc', as: 'irc'
     resources :admins
   end
 end
